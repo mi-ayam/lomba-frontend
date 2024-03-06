8fec30f412f00533e858722323847371c94dfebc
ScaferuZ <endrafruz@gmail.com>
2024-03-06 19:38:18 +0700

    feat: adding grid to parent div

--- src/components/ui/CompetitionCard.jsx
+++ src/components/ui/CompetitionCard.jsx
@@ -15,30 +15,36 @@ const CompetitionCard = () => {
 
   return (
     <>
-      {competitions.map((competition) => {
-        return (
-          <div key={competition.id} className="max-w-xs border border-fg-gray rounded-lg shadow ">
-            <Link to={"/competitions/" + competition.id}>
-              <img className="w-full" src={competition.image} alt="an image" />
-            </Link>
-            <div className="p-5">
-              <Link
-                to={"/competitions/" + competition.id}
-                className="inline-flex items-center px-2 py-1 my-2 text-sm font-medium text-center
-          bg-secondary-blue bg-opacity-5 text-secondary-blue rounded-lg "
-              >
-                {competition.category}
-              </Link>
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+        {competitions.map((competition) => {
+          return (
+            <div key={competition.id} className="max-w-xs border border-fg-gray rounded-lg shadow ">
               <Link to={"/competitions/" + competition.id}>
-                {/* title */}
-                <h5 className="text-2xl font-semibold tracking-tight text-white">
-                  {competition.title}
-                </h5>
+                <img
+                  className="w-full text-white flex justify-center items-center"
+                  src={image}
+                  alt="an image"
+                />
               </Link>
+              <div className="p-5">
+                <Link
+                  to={"/competitions/" + competition.id}
+                  className="inline-flex items-center px-2 py-1 my-2 text-sm font-medium text-center
+          bg-secondary-blue bg-opacity-5 text-secondary-blue rounded-lg "
+                >
+                  {competition.category}
+                </Link>
+                <Link to={"/competitions/" + competition.id}>
+                  {/* title */}
+                  <h5 className="text-2xl font-semibold tracking-tight text-white">
+                    {competition.title}
+                  </h5>
+                </Link>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </>
   );
 };
