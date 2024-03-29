c010798b94a5d39518c265ebd75d8ef05932fdf1
ScaferuZ <endrafruz@gmail.com>
2024-03-30 04:36:42 +0700

    feat: updating navbar to include a logo an button to login

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,20 +1,34 @@
 import { Link } from "react-router-dom";
 import Button from "../ui/Button";
+import Logo from "../../assets/ayoLombaLogo.png";
 
 const Navbar = () => {
   return (
-    <div className="flex px-6 items-center space-x-5">
+    <nav
+      className="flex w-full items-center justify-between
+      space-x-5 px-4"
+    >
       <Link to="/" className="text-white">
-        Home
+        <img className="w-20 h-20" src={Logo} alt="Logo" />
       </Link>
-      <Link to="/postCompetition">
-        <Button
-          value="Post Competition"
-          textColor="text-white"
-          backgroundColor="bg-secondary-blue"
-        />
-      </Link>
-    </div>
+      <div className="flex space-x-5">
+        <h2 className="text-white">Home</h2>
+        <h2 className="text-white">Competitions</h2>
+        <h2 className="text-white">About Us</h2>
+      </div>
+      <div className="space-x-5">
+        <Link to="">
+          <Button value="Login" textColor="text-white" backgroundColor="bg-secondary-blue" />
+        </Link>
+        <Link to="/postCompetition">
+          <Button
+            value="Post Competition"
+            textColor="text-white"
+            backgroundColor="bg-secondary-blue"
+          />
+        </Link>
+      </div>
+    </nav>
   );
 };
 
